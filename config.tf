e38b6aeb9f626dce04c7c023bc4ccac36477a178
abhinavdahiya <abhinav.dahiya@coreos.com>
2018-02-16 11:27:55 -0800

    config.tf update

--- config.tf
+++ config.tf
@@ -73,7 +73,7 @@ variable "tectonic_container_images" {
     bootkube                             = "quay.io/coreos/bootkube:v0.10.0"
     etcd                                 = "quay.io/coreos/etcd:v3.2.14"
     hyperkube                            = "quay.io/coreos/hyperkube:v1.9.1_coreos.0"
-    kube_core_renderer                   = "quay.io/coreos/kube-core-renderer-dev:6c49ce4da9fc36966812381891b4f558aa53097b"
+    kube_core_renderer                   = "quay.io/coreos/kube-core-renderer-dev:a02bfc1169738d422abff31fe653486c201b1f2b"
     kube_core_operator                   = "quay.io/coreos/kube-core-operator:beryllium-m1"
     tectonic_channel_operator            = "quay.io/coreos/tectonic-channel-operator:0.6.2"
     tectonic_prometheus_operator         = "quay.io/coreos/tectonic-prometheus-operator:v1.9.3"
