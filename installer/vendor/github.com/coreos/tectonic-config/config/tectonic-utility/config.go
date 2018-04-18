856f531a6a12ab5cbef77f960523837b3640173e
abhinavdahiya <abhinav.dahiya@coreos.com>
2018-04-18 11:10:13 -0700

    vendor: update

--- installer/vendor/github.com/coreos/tectonic-config/config/tectonic-utility/config.go
+++ installer/vendor/github.com/coreos/tectonic-config/config/tectonic-utility/config.go
@@ -13,46 +13,14 @@ const (
 
 // OperatorConfig defines the config for Tectonic Utility Operator.
 type OperatorConfig struct {
-	metav1.TypeMeta         `json:",inline"`
-	IdentityConfig          `json:"identityConfig"`
-	IngressConfig           `json:"ingressConfig"`
-	StatsEmitterConfig      `json:"statsEmitterConfig"`
-	TectonicConfigMapConfig `json:"tectonicConfigMap"`
-	NetworkConfig           `json:"networkConfig"`
-}
-
-// IdentityConfig defines the config for Tectonic Identity.
-type IdentityConfig struct {
-	AdminEmail        string `json:"adminEmail"`
-	AdminPasswordHash string `json:"adminPasswordHash"`
-	AdminUserID       string `json:"adminUserID"`
-	ConsoleClientID   string `json:"consoleClientID"`
-	ConsoleSecret     string `json:"consoleSecret"`
-	KubectlClientID   string `json:"kubectlClientID"`
-	KubectlSecret     string `json:"kubectlSecret"`
-}
-
-// IngressConfig defines the config for Tectonic Ingress.
-type IngressConfig struct {
-	ConsoleBaseHost string `json:"consoleBaseHost"`
-	IngressKind     string `json:"ingressKind"`
+	metav1.TypeMeta    `json:",inline"`
+	StatsEmitterConfig `json:"statsEmitterConfig"`
+	NetworkConfig      `json:"networkConfig"`
 }
 
 // StatsEmitterConfig defines the config for Tectonic Stats Emitter.
 type StatsEmitterConfig struct {
 	StatsURL string `json:"statsURL"`
-}
-
-// TectonicConfigMapConfig defines the variables that will be used by the Tectonic ConfigMap.
-type TectonicConfigMapConfig struct {
-	BaseAddress          string `json:"baseAddress"`
-	CertificatesStrategy string `json:"certificatesStrategy"`
-	ClusterID            string `json:"clusterID"`
-	ClusterName          string `json:"clusterName"`
-	IdentityAPIService   string `json:"identityAPIService"`
-	InstallerPlatform    string `json:"installerPlatform"`
-	KubeAPIServerURL     string `json:"kubeAPIserverURL"`
-	TectonicVersion      string `json:"tectonicVersion"`
 }
 
 // NetworkConfig holds information on cluster networking.
